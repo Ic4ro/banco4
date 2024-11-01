3ad502d648ad8d88b7e310e7cf5557bba1a528db
Ic4ro <161033248+Ic4ro@users.noreply.github.com>
2024-11-01 13:33:12 +0000

    add usuario_repository.py

--- app /repositories/usuario_repository.py
+++ app /repositories/usuario_repository.py
@@ -9,15 +9,13 @@ class UsuarioRepository:
     def salvar_usuario(self, usuario: Usuario):
         self.session.add(usuario)
         self.session.commit()
-        
 
     def pesquisar_usuario_por_email(self, email: str):
         return self.session.query(Usuario).filter_by(email=email).first()
 
     def deletar_usuario(self, usuario: Usuario):
-        self.session.add(usuario)
+        self.session.delete(usuario)
         self.session.commit()
-
 
     def listar_todos_usuarios(self):
         return self.session.query(Usuario).all()
